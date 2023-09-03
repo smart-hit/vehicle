4de81fba953a3b54498a994f24571c4c23e8e3d9
smart-hit <rohithp414@gmail.com>
2023-09-03 14:01:29 +0530

    commit

---
+++ src/pages/about us.js
@@ -0,0 +1,7 @@
+function about(){
+    return(
+        <p>Hey this is -----team,we are extremely delighted to help you rend a vehicle for you </p>
+    )
+}
+
+export default about;
---
+++ src/pages/home.js
@@ -0,0 +1,6 @@
+function Home(){
+    return(
+      <h1>rend a car of your dream</h1>
+    );
+}
+export default Home;
