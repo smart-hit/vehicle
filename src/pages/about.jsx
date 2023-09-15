83dffa8a7350c542de8b21937595067557c07553
smart-hit <rohithp414@gmail.com>
2023-09-15 19:11:57 +0530

    commit

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -5,8 +5,10 @@ function About(){
         <section>
         
         <div className="about-main">
-        <p>Hey this is -----team,we are extremely delighted to help you rend a vehicle for you </p>
-        
+            <div className="about-com">
+        <h1>About Company</h1>
+        <p>Hey this is RPcars team,we are extremely delighted to help you rend a vehicle for you.We offers a big range of vehicles for all your driving needs.We have the perfect car to meet your needs. </p>
+        </div>
         </div>
         </section>
         </>
